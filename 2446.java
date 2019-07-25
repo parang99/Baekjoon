e0fe138257aecb797c5f7ea6f4533bead541d9b6
parang99 <53161476+parang99@users.noreply.github.com>
2019-07-25 21:07:19 +0900

    Add files via upload

---
+++ 2446.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = n;i > 0;i--){
+            for(int j = 0;j < n - i;j++){
+                bw.write(" ");
+            }
+            for(int j = 0;j < 2 * i - 1;j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        
+        for(int i = 2;i <= n;i++){
+            for(int j = 0;j < n - i;j++){
+                bw.write(" ");
+            }
+            for(int j = 0;j < 2 * i - 1;j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
